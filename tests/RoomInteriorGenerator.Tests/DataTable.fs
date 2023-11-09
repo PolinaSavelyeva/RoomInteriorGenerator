49040799c050a2e90273a72b702a8add2e01ee2e
PolinaSavelyeva <lissava00@gmail.com>
2023-11-09 23:43:22 +0300

    Update DataTable.fs

--- tests/RoomInteriorGenerator.Tests/DataTable.fs
+++ tests/RoomInteriorGenerator.Tests/DataTable.fs
@@ -43,3 +43,38 @@ module GettingElementFromDataTable =
 
               testCase "Getting an element by an 0 index of non-empty DataTable will return the element"
               <| fun _ -> Expect.equal (dataTableOfLengthThree[0]) chairRow "The element at index 0 should be chairRow" ]
+
+module DeleteElementFromDataTable =
+    [<Tests>]
+    let tests =
+        testList
+            "delete element from DataTable"
+            [ testCase "Deleting an element by an index greater than the length of the DataTable will cause an error"
+              <| fun _ -> Expect.throws (fun _ -> dataTableOfLengthThree.Delete 12) "Index out of the range"
+
+              testCase "Deleting an element by an index equal to the length of the DataTable will cause an error"
+              <| fun _ -> Expect.throws (fun _ -> dataTableOfLengthOne.Delete 11) "Index out of the range"
+
+              testPropertyWithConfig config "Deleting an element by an index equal to the DataTable will cause an error on generated int DataTable"
+              <| fun (dataTable: IntDataTable) -> Expect.throws (fun _ -> dataTable.DataTable.Delete dataTable.Length) "Index out of the range"
+
+              testPropertyWithConfig config "Deleting an element by an index equal to the DataTable will cause an error on generated bool DataTable"
+              <| fun (dataTable: BoolDataTable) -> Expect.throws (fun _ -> dataTable.DataTable.Delete dataTable.Length) "Index out of the range"
+
+              testPropertyWithConfig config "Deleting an element by an index equal to the DataTable will cause an error on generated string DataTable"
+              <| fun (dataTable: StringDataTable) -> Expect.throws (fun _ -> dataTable.DataTable.Delete dataTable.Length) "Index out of the range"
+
+              testCase "Deleting an negative index of the DataTable will cause an error"
+              <| fun _ -> Expect.throws (fun _ -> dataTableOfLengthTwo.Delete -1) "Index out of the range"
+
+              testPropertyWithConfig config "Deleting an negative index of the DataTable will cause an error on generated int DataTable"
+              <| fun (dataTable: IntDataTable) (negativeInt: NegativeInt) -> Expect.throws (fun _ -> dataTable.DataTable.Delete negativeInt.Get) "Index out of the range"
+
+              testPropertyWithConfig config "Deleting an negative index of the DataTable will cause an error on generated bool DataTable"
+              <| fun (dataTable: BoolDataTable) (negativeInt: NegativeInt) -> Expect.throws (fun _ -> dataTable.DataTable.Delete negativeInt.Get) "Index out of the range"
+
+              testPropertyWithConfig config "Deleting an negative index of the DataTable will cause an error on generated string DataTable"
+              <| fun (dataTable: StringDataTable) (negativeInt: NegativeInt) -> Expect.throws (fun _ -> dataTable.DataTable.Delete negativeInt.Get) "Index out of the range"
+
+              testCase "Deleting an element by an 0 index of non-empty DataTable will work correctly"
+              <| fun _ -> Expect.equal (dataTableOfLengthTwo.Delete 0) () "The element at index 0 delete successfully" ]
