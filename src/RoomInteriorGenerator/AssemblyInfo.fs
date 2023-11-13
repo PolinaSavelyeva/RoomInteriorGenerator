d81a896a9caa3a83dedd83c38bd1db7393bf993d
PolinaSavelyeva <lissava00@gmail.com>
2023-11-13 23:30:16 +0300

    Revert "Bump version to 1.0.0-alpha2"
    This reverts commit b366bfb4818ce603e9bf89cfb91f69a621678645.

--- src/RoomInteriorGenerator/AssemblyInfo.fs
+++ src/RoomInteriorGenerator/AssemblyInfo.fs
@@ -9,7 +9,7 @@ open System.Reflection
 [<assembly: AssemblyFileVersionAttribute("1.0.0")>]
 [<assembly: AssemblyInformationalVersionAttribute("1.0.0")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseChannel","alpha")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","31fffc4e49f304c162204c803885ba3e4bc31de0")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","a51c1e11892e6e55febc38e837694e0b45f2476a")>]
 do ()
 
 module internal AssemblyVersionInformation =
@@ -20,4 +20,4 @@ module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyFileVersion = "1.0.0"
     let [<Literal>] AssemblyInformationalVersion = "1.0.0"
     let [<Literal>] AssemblyMetadata_ReleaseChannel = "alpha"
-    let [<Literal>] AssemblyMetadata_GitHash = "31fffc4e49f304c162204c803885ba3e4bc31de0"
+    let [<Literal>] AssemblyMetadata_GitHash = "a51c1e11892e6e55febc38e837694e0b45f2476a"
