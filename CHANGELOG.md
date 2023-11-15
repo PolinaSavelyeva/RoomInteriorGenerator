050e6decd9e4d0366e4cb9e0e544ddc4740852e0
PolinaSavelyeva <lissava00@gmail.com>
2023-11-15 17:59:23 +0300

    Bump version to 1.0.0-alpha3
    ## [1.0.0-alpha3] - 2023-11-15
    
    ### Fixed
    - Fix placement function signature to prevent C# errors
    
    ### Added
    - Add basic logic
    [Unreleased]: https://github.com/PolinaSavelyeva/RoomInteriorGenerator/compare/v1.0.0-alpha1...HEAD
    [1.0.0-alpha1]: https://github.com/PolinaSavelyeva/RoomInteriorGenerator/releases/tag/v1.0.0-alpha1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,13 +1,19 @@
 # Changelog
 
-## [Unreleased]
+## [1.0.0-alpha3] - 2023-11-15
 
 ### Fixed
 - Fix placement function signature to prevent C# errors
+
+### Added
+- Add basic logic
+[Unreleased]: https://github.com/PolinaSavelyeva/RoomInteriorGenerator/compare/v1.0.0-alpha1...HEAD
+[1.0.0-alpha1]: https://github.com/PolinaSavelyeva/RoomInteriorGenerator/releases/tag/v1.0.0-alpha1
 
 ## [1.0.0-alpha1] - 2023-11-13
 
 ### Added
 - Add basic logic
-[Unreleased]: https://github.com/PolinaSavelyeva/RoomInteriorGenerator/compare/v1.0.0-alpha1...HEAD
+[Unreleased]: https://github.com/PolinaSavelyeva/RoomInteriorGenerator/compare/v1.0.0-alpha3...HEAD
+[1.0.0-alpha3]: https://github.com/PolinaSavelyeva/RoomInteriorGenerator/releases/tag/v1.0.0-alpha3
 [1.0.0-alpha1]: https://github.com/PolinaSavelyeva/RoomInteriorGenerator/releases/tag/v1.0.0-alpha1
--- src/RoomInteriorGenerator/AssemblyInfo.fs
+++ src/RoomInteriorGenerator/AssemblyInfo.fs
@@ -5,19 +5,19 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("RoomInteriorGenerator")>]
 [<assembly: AssemblyProductAttribute("RoomInteriorGenerator")>]
 [<assembly: AssemblyVersionAttribute("1.0.0")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","2023-11-13T00:00:00.0000000+03:00")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","2023-11-15T00:00:00.0000000+03:00")>]
 [<assembly: AssemblyFileVersionAttribute("1.0.0")>]
 [<assembly: AssemblyInformationalVersionAttribute("1.0.0")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseChannel","alpha")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","b898e9a7428b517cc550b3d365303dee5637e0b3")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","848d86f1527c275e7538841d4681ac7b2503dc48")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "RoomInteriorGenerator"
     let [<Literal>] AssemblyProduct = "RoomInteriorGenerator"
     let [<Literal>] AssemblyVersion = "1.0.0"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "2023-11-13T00:00:00.0000000+03:00"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "2023-11-15T00:00:00.0000000+03:00"
     let [<Literal>] AssemblyFileVersion = "1.0.0"
     let [<Literal>] AssemblyInformationalVersion = "1.0.0"
     let [<Literal>] AssemblyMetadata_ReleaseChannel = "alpha"
-    let [<Literal>] AssemblyMetadata_GitHash = "b898e9a7428b517cc550b3d365303dee5637e0b3"
+    let [<Literal>] AssemblyMetadata_GitHash = "848d86f1527c275e7538841d4681ac7b2503dc48"
